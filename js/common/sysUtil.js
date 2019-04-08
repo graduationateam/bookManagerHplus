5dc899d664b477d7de61baf3267f34e965e13897
zhangheng <1334180712@qq.com>
2019-04-08 12:42:47 +0800

    101后台

---
+++ js/common/sysUtil.js
@@ -0,0 +1,197 @@
+var sysUtils = $.extend({}, sysUtils); //全局对象
+
+sysUtils.message = function(title) {
+	$.messager.show({
+		msg : title,
+		title : '提示'
+	});
+};
+
+sysUtils.bigmessage = function(title) {
+	$.messager.show({
+		width:300,
+		height:150,
+		msg : title,
+		timeout:5000,
+		title : '提示'
+	});
+};
+
+sysUtils.alert = function(title, msg, icon, callback) {
+	var icontype = ["error","info","question","warning"];
+	$.messager.defaults = {ok: "确定",cancel: "取消",width:300,height:"auto",minHeight:150,modal:true,collapsible:false,minimizable:false,maximizable:false,resizable:false,closable:false};
+	$.messager.alert(title, msg, icontype[icon], callback);
+};
+
+sysUtils.confirm = function(title, msg, callback) {
+	$.messager.defaults = {ok: "确定",cancel: "取消",width:300,height:"auto",minHeight:150,modal:true,collapsible:false,minimizable:false,maximizable:false,resizable:false,closable:false};
+	$.messager.confirm(title, msg, callback);
+};
+
+sysUtils.dialog = function(options) {
+	var bodyCls = 'dialog-body';
+	if (options.bodyCls != undefined) {
+		bodyCls = options.bodyCls;
+	}
+	var opts = $.extend({
+		modal : true,
+		cache: false,
+	    bodyCls: bodyCls,
+	    loadingMessage: '加载中',
+		onClose : function() {
+			$(this).dialog('destroy');
+		}
+	}, options);
+	return $('<div/>').dialog(opts)
+};
+
+sysUtils.imgDialog = function(obj) {
+	var dialog = $('<div style="background-image: url('+$(obj)[0].currentSrc+')"></div>').dialog({
+		title: '',
+	    border: 0,
+	    width: $(obj)[0].naturalWidth,
+	    height: $(obj)[0].naturalHeight,
+	    closed: false,
+	    cache: false,
+	    //href: $(obj)[0].currentSrc,
+	    modal: true
+	});
+	
+	$('.window-mask').click(function() {
+		dialog.dialog('close');
+	});
+};
+
+sysUtils.ajaxPost = function(url, data, callback) {
+	$.ajax({
+		url : url,
+		type:"post",
+		data : data,
+		cache : false,
+		dataType : "json",
+		success : function(response) {
+			callback(response);
+		}
+	});
+};
+
+sysUtils.TbAjaxPost = function(url, data, callback) {
+	$.ajax({
+		url : url,
+		type:"post",
+		data : data,
+		cache : false,
+		async : false,
+		dataType : "json",
+		success : function(response) {
+			callback(response);
+		}
+	});
+};
+
+sysUtils.ajaxGet = function(url, data, callback) {
+	$.ajax({
+		url : url,
+		type:"get",
+		data : data,
+		cache : false,
+		dataType : "json",
+		success : function(response) {
+			callback(response);
+		}
+	});
+};
+
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
+
+var Clock = function() {
+	var date = new Date();
+	this.year = date.getFullYear();
+	this.month = date.getMonth() + 1;
+	this.date = date.getDate();
+	this.day = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六")[date.getDay()];
+	this.hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+	this.minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+	this.second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+	this.toString = function() {
+		return "" + this.year + "年" + this.month + "月" + this.date + "日 " + this.hour + ":" + this.minute + ":" + this.second + " " + this.day; 
+	};
+	
+	this.toSimpleDate = function() {
+		return this.year + "-" + this.month + "-" + this.date;
+	};
+	
+	this.toDetailDate = function() {
+		return this.year + "-" + this.month + "-" + this.date + " " + this.hour + ":" + this.minute + ":" + this.second;
+	};
+	
+	this.display = function(ele) {
+		var clock = new Clock();
+		ele.innerHTML = clock.toString();
+		window.setTimeout(function() {clock.display(ele);}, 1000);
+	};
+};
+
+sysUtils.imgSrcError = function(src){
+    var img = event.srcElement;
+    img.src = src;
+    img.onerror = null;
+};
+
+/**
+ * 获取地址栏参数
+ * @param name
+ * @returns {*}
+ */
+sysUtils.getAttribute = function(name) {
+//	var obj = {};
+//	var svalue = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]*)(\&?)","i"));
+//	obj[name] = svalue ? decodeURI(svalue[1]) : decodeURI(svalue);
+//	return obj;
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
+        r = window.location.search.substr(1).match(reg),
+        qs = '';
+    if (r != null) qs = decodeURIComponent(r[2]);
+    return qs;
+};
+
+sysUtils.putStorage = function(name, value) {
+    localStorage.setItem(name, value);
+};
+
+sysUtils.getStorage = function(name) {
+    var value = null;
+    value = localStorage.getItem(name);
+    return value;
+};
+
+sysUtils.removeStorage = function(name) {
+    localStorage.removeItem(name);
+};
+
+sysUtils.clearStorage = function() {
+    localStorage.clear();
+};
+
+sysUtils.isNull = function(val) {
+    if (val == "" || val == null || val == undefined || val === "" || val === null || val === undefined){
+    	return true;
+	}  else {
+    	return false;
+	}
+};
---
+++ js/order/order.js
@@ -0,0 +1,166 @@
+
+function operateFormatter(value, row, index) {//赋予的参数
+    return [
+        '<a class="btn btn-default" href="#">删除</a>'
+        
+    ].join('');
+}
+
+var $table;
+  
+//初始化bootstrap-table的内容
+function InitMainTable () {
+//记录页面bootstrap-table全局变量$table，方便应用
+var queryUrl = 'http://localhost:8080/Manager/userModule/toList';
+ $('#orderListTable').bootstrapTable({
+    url: queryUrl,                      //请求后台的URL（*）
+    dataType:"json",
+    method: 'post',                      //请求方式（*）
+    //toolbar: '#toolbar',              //工具按钮用哪个容器
+    striped: true,                      //是否显示行间隔色
+    cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+    pagination: true,                   //是否显示分页（*）
+    sortable: true,                     //是否启用排序
+    sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+    pageNumber: 1,                      //初始化加载第一页，默认第一页,并记录
+    pageSize: 10,                     //每页的记录行数（*）
+    pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+    search: false,                      //是否显示表格搜索
+    strictSearch: true,
+    showColumns: true,                  //是否显示所有的列（选择显示的列）
+    showRefresh: true,                  //是否显示刷新按钮
+    minimumCountColumns: 2,             //最少允许的列数
+    clickToSelect: true,                //是否启用点击选中行
+    //height: 500,                      //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+    uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+    showToggle: true,                   //是否显示详细视图和列表视图的切换按钮
+    cardView: false,                    //是否显示详细视图
+    detailView: false,                  //是否显示父子表
+    contentType: 'application/x-www-form-urlencoded',
+    //得到查询的参数
+    queryParams : function (params) {
+        console.log(params);
+        //这里的键的名字和控制器的变量名必须一致，这边改动，控制器也需要改成一样的
+        var temp = {   
+            limit: params.limit,                         //页面大小
+            page: (params.offset / params.limit) + 1,   //页码
+          
+        };
+        console.log(temp);
+        return temp;
+    },
+    //queryParamsType:'',
+    columns: [{
+        checkbox: true,  
+        visible: true                  //是否显示复选框  
+    }, {
+        field: 'order_number',
+        title: '订单编号',
+        sortable: true
+    }, {
+        field: 'name',
+        title: '书籍名称',
+        sortable: true
+    }, {
+        field: 'seller',
+        title: '卖家',
+        sortable: true,
+    },
+    {
+        field: 'buyer',
+        title: '买家',
+        sortable: true
+    }, 
+    {
+        field: 'order_amount',
+        title: '订单金额',
+        sortable: true
+    }, 
+    {
+        field: 'state',
+        title: '订单状态',
+        sortable: true
+    }, 
+    {
+        field: 'time',
+        title: '下单时间',
+        sortable: true
+    }, 
+    {
+        field:'id',
+        title: '操作',
+        width: 120,
+        align: 'center',
+        valign: 'middle',
+        formatter: operateFormatter
+    }, ],
+    onLoadSuccess: function () {
+    },
+    onLoadError: function () {
+        showTips("数据加载失败！");
+    },
+    onDblClickRow: function (row, $element) {
+        var id = row.id;
+        console.log(id);
+        //EditViewById(id, 'view');
+    }, 
+    responseHandler:function(res){
+    //在ajax获取到数据，渲染表格之前，修改数据源
+    //console.log(res);
+    var t_data=[];
+    if(res.code==1){
+        t_data = {total:res.data.total,rows:res.data.list};
+    }
+    else{
+        t_data = {total:0,rows:[]};
+    }
+    //console.log(t_data);
+    return t_data;
+    } 
+});
+};
+
+$('#orderListTable').on('all.bs.table', function(e, name, args) {
+    console.log('Event:', name, ', data:', args);
+  })
+  .on('click-row.bs.table', function(e, row, $element) {
+    //console.log('Event: click-row.bs.table');
+  })
+  .on('dbl-click-row.bs.table', function(e, row, $element) {
+    //console.log('Event: dbl-click-row.bs.table');
+  })
+  .on('sort.bs.table', function(e, name, order) {
+    //console.log('Event: sort.bs.table');
+  })
+  .on('check.bs.table', function(e, row) {
+   //console.log('Event: check.bs.table');
+   alert(row.id);
+  })
+  .on('uncheck.bs.table', function(e, row) {
+   // console.log('Event: uncheck.bs.table');
+   alert(row.id);
+  })
+  .on('check-all.bs.table', function(e) {
+   // console.log('Event: check-all.bs.table');
+   
+  })
+  .on('uncheck-all.bs.table', function(e) {
+   // console.log('Event: uncheck-all.bs.table');
+  })
+  .on('load-success.bs.table', function(e, data) {
+   // console.log('Event: load-success.bs.table');
+  })
+  .on('load-error.bs.table', function(e, status) {
+   // console.log('Event: load-error.bs.table');
+  })
+  .on('column-switch.bs.table', function(e, field, checked) {
+   // console.log('Event: column-switch.bs.table');
+  })
+  .on('page-change.bs.table', function(e, size, number) {
+  // console.log('Event: page-change.bs.table');
+  })
+  .on('search.bs.table', function(e, text) {
+    //console.log('Event: search.bs.table');
+  });
+    
+    
